8b6fbbd98306c6c7b20e3f098871738873d5ed5d
Bagai Glory <bagaiglory@gmail.com>
2025-01-27 22:42:32 +0100

    Initial commit with password generator

--- Task3.py
+++ Task3.py
@@ -1,24 +1,35 @@
 import random
 # password generator
 
-password_length = input("What is the lenght of the email adress you want")
+password_length = int(input("What is the lenght of the email adress you want? \n"))
+
 uppercase_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
                      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lowercase_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = [0,1,2,3,4,5,6,7,8,9]
-keyboard_symbols = [
+symbols = [
     "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", 
     "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", 
     ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?", 
     "~", "`"]
 
-
+ 
 def password_generator(password_length):
-    '''The function generates a password based on the user's required length'''
     pw = []
-    if "@" and ".com" in email_adress:
-        print("This is a valid email adress")
-    else:
-        print(f"The generate password is '{pw}'")
+    while len(pw)<password_length:
+        pw.append(random.choice(uppercase_letter))
+        if len(pw) < password_length:
+            pw.append(random.choice(lowercase_letter))
+        if len(pw) < password_length:
+            pw.append(random.choice(numbers))   
+        if len(pw) < password_length:
+            pw.append(random.choice(symbols))
+    "________________________________________________________"
 
+    random.shuffle(pw)
+    return ''.join(map(str, pw[:password_length]))
+
+print(f" The required password is '{password_generator(password_length)}'" )
+    
+#print(password_generator(password_length))
